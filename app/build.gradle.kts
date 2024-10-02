faa58131181f70924470b2eac781e2114e54cd78
25748 <2574822036@qq.com>
2024-10-02 15:33:09 +0800

    --更新 #更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.chaos.hyperstar"
         minSdk = 33
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

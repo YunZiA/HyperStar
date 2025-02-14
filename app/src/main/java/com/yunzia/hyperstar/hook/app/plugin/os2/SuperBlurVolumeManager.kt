8d117effd4538658a892cbf96c2526edb3335877
25748 <2574822036@qq.com>
2025-02-15 02:57:30 +0800

    --补充提交

---
+++ app/src/main/java/com/yunzia/hyperstar/hook/app/plugin/os2/SuperBlurVolumeManager.kt
@@ -0,0 +1,32 @@
+package com.yunzia.hyperstar.hook.app.plugin.os2
+
+import android.content.Context
+import com.yunzia.hyperstar.hook.base.Hooker
+import com.yunzia.hyperstar.utils.XSPUtils
+
+class SuperBlurVolumeManager : Hooker() {
+
+    val superBlurVolume = XSPUtils.getInt("is_super_blur_volume",0)
+
+    override fun initHook(classLoader: ClassLoader?) {
+        super.initHook(classLoader)
+        if (superBlurVolume != 0){
+            startMethodsHook()
+        }
+    }
+
+    private fun startMethodsHook() {
+        findClass(
+            "miui.systemui.util.MiBlurCompat",
+            classLoader
+        ).afterHookMethod("getBackgroundBlurOpenedInDefaultTheme",Context::class.java){
+
+            if (superBlurVolume == 1){
+                it.result = false
+            }else if (superBlurVolume == 2){
+                it.result = true
+            }
+        }
+
+    }
+}

640269cd9a308bf4403c78f5f4c3c2d230c05bab
25748 <2574822036@qq.com>
2025-04-20 20:52:21 +0800

    --update build.gradle.kts --update dev.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,19 +81,16 @@ android {
 
     val keystoreFile = System.getenv("KEYSTORE_PATH")
     signingConfigs {
-        if (keystoreFile != null) {
-            create("ci") {
+        create("dev") {
+            if (keystoreFile != null) {
                 storeFile = file(keystoreFile)
                 storePassword = System.getenv("KEYSTORE_PASSWORD")
                 keyAlias = System.getenv("KEY_ALIAS")
                 keyPassword = System.getenv("KEY_PASSWORD")
-                enableV4Signing = true
             }
-        } else {
-            create("release") {
-                enableV4Signing = true
-            }
+            enableV4Signing = true
         }
+
     }
 
     applicationVariants.all {

ab51f1c6ebaaea462131c31a1a271ec13f88ff6a
25748 <2574822036@qq.com>
2025-10-01 09:29:38 +0800

    #OS3 --修复某个版本开始的验证码自动复制失效

--- app/src/main/java/com/yunzia/hyperstar/hook/app/mms/AutoCopyVerificationCode.kt
+++ app/src/main/java/com/yunzia/hyperstar/hook/app/mms/AutoCopyVerificationCode.kt
@@ -5,62 +5,52 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.os.PersistableBundle
 import com.yunzia.hyperstar.hook.base.Hooker
 import com.yunzia.hyperstar.utils.XSPUtils
 
 
 class AutoCopyVerificationCode: Hooker() {
-
     val autoCopyVerificationCode = XSPUtils.getBoolean("auto_copy_verification_code",false)
-
 
     override fun initHook(classLoader: ClassLoader?) {
         super.initHook(classLoader)
-
         if (!autoCopyVerificationCode) return
-//        findClass(
-//            "com.android.mms.transaction.i\$c",
-//            classLoader
-//        ).afterHookMethod(
-//            "a",
-//            Context::class.java,
-//            Boolean::class.java,
-//            Boolean::class.java,
-//        ){
-//            val itemExtra = this.getObjectField("l")?:return@afterHookMethod
-//            val code = itemExtra.callMethod("getOTP") as String
-//            val context = it.args[0] as Context
-//            context.copyClipboard(code)
-//            starLog.log("getOTP:$code")
-//
-//        }
-
-
-        PendingIntent::class.java.afterHookMethod(
-            "getActivity",
-            Context::class.java,
-            Int::class.java,
-            Intent::class.java,
-            Int::class.java
-        ){
-            val intent = it.args[2] as Intent
-            val extraText = intent.getStringExtra("extra_text")
-            if (extraText != null) {
+        PendingIntent::class.java.apply {
+            afterHookMethod(
+                "getActivity",
+                Context::class.java,
+                Int::class.java,
+                Intent::class.java,
+                Int::class.java
+            ){
+                val intent = it.args[2] as Intent
+                val extraText = intent.getStringExtra("extra_text") ?: return@afterHookMethod
                 val context = it.args[0] as Context
                 context.copyVerificationCodeToClipboard(extraText)
                 logD("New verification code: $extraText")
-
+            }
+            afterHookMethod(
+                "getActivity",
+                Context::class.java,
+                Int::class.java,
+                Intent::class.java,
+                Int::class.java,
+                Bundle::class.java
+            ){
+                val intent = it.args[2] as Intent
+                val extraText = intent.getStringExtra("extra_text") ?: return@afterHookMethod
+                val context = it.args[0] as Context
+                context.copyVerificationCodeToClipboard(extraText)
+                logD("New verification code: $extraText")
             }
         }
-
-
     }
 
 
     private fun Context.copyVerificationCodeToClipboard(text: CharSequence) {
-        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager
-            ?: return
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager ?: return
         val extras = PersistableBundle().apply {
             putBoolean("mms_is_ververification_code", true)
         }
